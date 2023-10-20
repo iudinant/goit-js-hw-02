9fad5a43046e10a94247b10354e9496fbed0556f
iudinant <131783168+iudinant@users.noreply.github.com>
2023-10-20 09:24:23 +0200

    Update task4.js

--- js/task4.js
+++ js/task4.js
@@ -27,3 +27,9 @@ function getShippingCost(country){
 
 }
 
+console.log(getShippingCost("Australia"));
+console.log(getShippingCost("Germany"));
+console.log(getShippingCost("China"));
+console.log(getShippingCost("Chile"));
+console.log(getShippingCost("Jamaica"));
+console.log(getShippingCost("Sweden"));
